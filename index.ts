abd10423043b49dd39b77e2e92b18b58f9abe3f3
JuanoBJ <alfonsinjuanleandro@gmail.com>
2023-03-22 01:56:45 -0300

    correcion black

--- index.ts
+++ index.ts
@@ -1,11 +1,16 @@
-import { Casino } from "./Casino";
-import { Tragamonedas } from "./Tragamonedas";
-import { Ruleta } from "./Ruleta";
-import { Dados } from "./Dados";
+import { Casino } from './Casino';
+import { Dados } from './Dados';
+import { Ruleta } from './Ruleta';
+import { Tragamonedas } from './Tragamonedas';
 
-const miCasino = new Casino();
-miCasino.agregarJuego(new Tragamonedas());
-miCasino.agregarJuego(new Ruleta());
-miCasino.agregarJuego(new Dados());
+const casino = new Casino();
 
-miCasino.jugarTodos();
+const juego1 = new Dados();
+const juego2 = new Ruleta();
+const juego3 = new Tragamonedas();
+
+casino.agregarJuego(juego1);
+casino.agregarJuego(juego2);
+casino.agregarJuego(juego3);
+
+console.log(casino.jugarTodos());
