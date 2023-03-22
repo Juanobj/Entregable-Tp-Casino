c932269a960e84415c8a8b3b93a0d274886edfda
JuanoBJ <alfonsinjuanleandro@gmail.com>
2023-03-22 01:59:38 -0300

    cambio en index

--- index.ts
+++ index.ts
@@ -1,16 +1,33 @@
-import { Casino } from './Casino';
-import { Dados } from './Dados';
-import { Ruleta } from './Ruleta';
-import { Tragamonedas } from './Tragamonedas';
+import { Ruleta } from "./Ruleta";
+import { Tragamonedas } from "./Tragamonedas";
+import { Dados } from "./Dados";
+
+class Casino {
+  private juegos: any[];
+
+  constructor() {
+    this.juegos = [];
+  }
+
+  public agregarJuego(juego: any): void {
+    this.juegos.push(juego);
+  }
+
+  public mostrarJuegos(): void {
+    console.log("Juegos disponibles:");
+    this.juegos.forEach((juego) => {
+      console.log(juego.nombre);
+    });
+  }
+}
 
 const casino = new Casino();
+const ruleta = new Ruleta();
+const tragamonedas = new Tragamonedas();
+const dados = new Dados();
 
-const juego1 = new Dados();
-const juego2 = new Ruleta();
-const juego3 = new Tragamonedas();
+casino.agregarJuego(ruleta);
+casino.agregarJuego(tragamonedas);
+casino.agregarJuego(dados);
 
-casino.agregarJuego(juego1);
-casino.agregarJuego(juego2);
-casino.agregarJuego(juego3);
-
-console.log(casino.jugarTodos());
+casino.mostrarJuegos();
